365c016615ecb3e8f776756b8630d1cccc7d13b2
seeyuyu <seeyuyu@163.com>
2021-01-27 20:49:34 +0800

    feat: 滑动窗口法处理

--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -9,8 +9,20 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
+  let left = 0, right = 0, res = 0;
+  let window = {};
 
+  while (right < s.length) {
+    let cur = s[right++]
+    window[cur] ? window[cur]++ : window[cur] = 1
+    while (window[cur] > 1) {
+      let d = s[left++]
+      window[d] ? window[d]-- : window[d] = 0
+    }
+    res = Math.max(res, right - left)
+  }
+  return res
 };
 // @lc code=end
 
