1dcf5cb9301435760eda23cc9bfbf95e6df55d97
seeyuyu <seeyuyu@163.com>
2021-01-28 20:35:28 +0800

    124,后序遍历二叉树

---
+++ 124.二叉树中的最大路径和.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=124 lang=javascript
+ *
+ * [124] 二叉树中的最大路径和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function (root) {
+  let res = -Infinity
+  function getMax(root) {
+    if (!root) {
+      return 0
+    }
+    let left = Math.max(0, getMax(root.left))
+    let right = Math.max(0, getMax(root.right))
+    res = Math.max(res, left + right + root.val)
+    return root.val + Math.max(left, right) 
+  }
+  getMax(root)
+  return res
+};
+// @lc code=end
+
