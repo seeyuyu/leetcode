c46b90bb6efe64cd0725cb7cf91f5b0c8833e250
李栋焱 <lidongyan@9N92PW2.corp.qihoo.net>
2021-02-01 13:03:48 +0800

    refactor: 92反转链表迭代法加虚拟节点优化

--- 92.反转链表-ii.js
+++ 92.反转链表-ii.js
@@ -83,27 +83,76 @@
 // 复杂问题拆分为简单问题，一个函数只做一个事情
 // 链表分为了3段，前驱段，反转段，后继段
 // 前驱段为0,m-1;反转段为m,n;后继段为n+1,length
+// var reverseBetween = function (head, m, n) {
+//   if (m === n) return head
+//   let start = head;
+//   for (let i = 1; i < m - 1; i++) {
+//     start = start.next
+//     n--
+//   }
+//   // let finish = null
+//   // let cur = null, pre = start;
+//   console.log('start.next , n is ', start.next, n)
+//   // start为前驱节点的end, 它要接入后面的值，
+//   if (m === 1) {
+//     head = reverse(start, n)
+//     console.log('m===1', head)
+//     return head
+//   } else {
+//     // 这里之所以减一，是因为相对于start是n,那么相当于start.next,则是n-1
+//     start.next = reverse(start.next, n - 1)
+//     console.log('head is', head)
+//     return head
+//   }
+// }
+// /**
+//  * 当前节点，反转前几位数字
+//  * @param {*} head 
+//  * @param {*} n 
+//  */
+// function reverse(head, n) {
+//   let tail = null, cur = null, pre = head
+//   while (n > 0 && pre) {
+//     if (n === 1) {
+//       tail = pre.next
+//       console.log('tail is ', tail)
+//     }
+//     let temp = pre.next
+//     pre.next = cur
+//     cur = pre
+//     pre = temp
+//     n--
+//   }
+//   console.log('tail, cur, pre, head is ,', tail, cur, pre, head)
+//   head.next = tail
+//   return cur
+// }
+
+// 此处的前驱情况有两种，一种是有前驱，一种没前驱，如果用m===1来判断，有两种情况，如果给其
+// 加一个虚拟节点，那么就成了一种情况，肯定都有前驱，至少是一个空节点，这样处理会少一种情况，便于调试
 var reverseBetween = function (head, m, n) {
   if (m === n) return head
-  let start = head;
-  for (let i = 1; i < m - 1; i++) {
-    start = start.next
+  let start = new ListNode(-1)
+  start.next = head;
+  let node = start
+  for (let i = 1; i < m; i++) {
+    node = node.next
     n--
   }
-  // let finish = null
-  // let cur = null, pre = start;
-  console.log('start.next , n is ', start.next, n)
+  console.log('start.next , n is ', node.next, n)
+  node.next = reverse(node.next, n)
+  return start.next
   // start为前驱节点的end, 它要接入后面的值，
-  if (m === 1) {
-    head = reverse(start, n)
-    console.log('m===1', head)
-    return head
-  } else {
-    // 这里之所以减一，是因为相对于start是n,那么相当于start.next,则是n-1
-    start.next = reverse(start.next, n - 1)
-    console.log('head is', head)
-    return head
-  }
+  // if (m === 1) {
+  //   head = reverse(start, n)
+  //   console.log('m===1', head)
+  //   return head
+  // } else {
+  //   // 这里之所以减一，是因为相对于start是n,那么相当于start.next,则是n-1
+  //   start.next = reverse(start.next, n - 1)
+  //   console.log('head is', head)
+  //   return head
+  // }
 }
 /**
  * 当前节点，反转前几位数字
@@ -127,6 +176,7 @@ function reverse(head, n) {
   head.next = tail
   return cur
 }
+
 // @lc code=end
 
 // "[3,5] \n 1 \n2"
