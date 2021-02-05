fa604f89b93f24f91385ed4ce090dc8aa84c698a
李栋焱 <lidongyan@9N92PW2.corp.qihoo.net>
2021-02-05 11:27:43 +0800

    feat: 234,链表的后续遍历向中夹逼,或者快慢指针找到中点,向两侧扩散对比

--- 234.回文链表.ts
+++ 234.回文链表.ts
@@ -23,22 +23,90 @@ interface ListNode {
   next: ListNode | null
 }
 
+// function isPalindrome(head: ListNode | null): boolean {
+//   if (!head) {
+//     return true
+//   }
+//   let oldHead = copyLinkList(head)
+//   let newHead = reverse(head)
+//   while (oldHead && newHead) {
+//     // 
+//     if (oldHead.val != newHead.val) {
+//       return false
+//     }
+//     oldHead = oldHead.next
+//     newHead = newHead.next
+//   }
+//   return true
+// };
+// function reverse(head: ListNode | null): ListNode | null {
+//   let cur = null, pre = head, temp = null
+//   while (pre) {
+//     temp = pre.next
+//     pre.next = cur
+//     cur = pre
+//     pre = temp
+//   }
+//   head.next = null
+//   return cur
+// }
+// function copyLinkList(head) {
+//   let start = head
+//   let copyLinkList = new ListNode(-1)
+//   let copyHead = copyLinkList
+//   while (start) {
+//     copyLinkList.next = new ListNode(start.val)
+//     start = start.next
+//     copyLinkList = copyLinkList.next
+//   }
+//   return copyHead.next
+// }
+
+// 深度遍历法-后序遍历链表，到最后一位，然后向中间夹逼
+// let left
+// function isPalindrome(head: ListNode | null): boolean {
+//   left = head
+//   return contrast(head)
+// }
+// function contrast(right) {
+//   if (!right) return true
+//   let res = contrast(right.next)
+//   res = res && (right.val === left.val)
+//   left = left.next
+//   return res
+// }
+
+/**
+ * 此方法为找到中位值，然后中位值向左右扩散对比值，时间复杂度为N,空间复杂度也为n
+ * 如果不能修改原链表结构，可以引入新的指针来对比，return之前再次反转反转过得后半截链表
+ * @param head 
+ */
 function isPalindrome(head: ListNode | null): boolean {
-  if (!head) {
-    return true
+  if (!head || !head.next) return true
+  let newHead = new ListNode(-1)
+  newHead.next = head
+  let fast = newHead, slow = newHead
+  while (fast && fast.next) {
+    fast = fast.next.next
+    slow = slow.next
   }
-  let oldHead = copyLinkList(head)
-  let newHead = reverse(head)
-  while (oldHead && newHead) {
-    // console.log('oldHead, newHead is', oldHead, newHead)
-    if (oldHead.val != newHead.val) {
+  // 
+  // 如果是fast存在，说明是偶数长度如果不存在是奇数
+  // 如果是奇数，那说明slow指的是正中间，需要向后继挪位置，以比较两段是否对称
+  if (!fast) slow = slow.next
+
+  slow = reverse(slow)
+  // 
+  while (head&& slow) {
+    if (head.val !== slow.val) {
       return false
     }
-    oldHead = oldHead.next
-    newHead = newHead.next
+    head = head.next
+    slow = slow.next
   }
+  
   return true
-};
+}
 function reverse(head: ListNode | null): ListNode | null {
   let cur = null, pre = head, temp = null
   while (pre) {
@@ -50,20 +118,8 @@ function reverse(head: ListNode | null): ListNode | null {
   head.next = null
   return cur
 }
-function copyLinkList(head) {
-  let start = head
-  let copyLinkList = new ListNode(-1)
-  let copyHead = copyLinkList
-  while (start) {
-    copyLinkList.next = new ListNode(start.val)
-    start = start.next
-    copyLinkList = copyLinkList.next
-  }
-  return copyHead.next
-}
-
 // "[1,1,2,1]"
 // "[1,2,3,2,1]"
-// "[1,2,1]"
+// "[1,2,1]" "[0,0]"
 // @lc code=end
 
