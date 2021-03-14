23717acf31d12ce1766b48fe51b4b697a257b773
seeyuyu <seeyuyu@163.com>
2021-03-14 11:11:20 +0800

    style: 72编辑距离,增加注释

--- 72.编辑距离.ts
+++ 72.编辑距离.ts
@@ -11,8 +11,15 @@ function minDistance(word1: string, word2: string): number {
   // let memo = new Array(word1.length).fill(new Array(word2.length))
   // console.log('memo is ', memo)
   function dp(i, j) {
+    // basecase解析： 当dp[i][j]某一个到达边界，那么word1[0,i]和word2[0,j]的编辑距离为剩下的长度，
+    // 也就是i== -1时候，需要在增加j个单位，因为i== -1，需要，j补上一个单位,即j+1
     if (i === -1) return j + 1
     if (j === -1) return i + 1
+    // 这种处理方式不行，因为此处的i,j表示的
+    // 当i==0，和j==0的时候，依然没有对比完成，0是数组第一位的索引，所以不能用此来结束
+    // 因为dp(i, j)  表示 返回 world1[0..i] 和 world2[0..j] 的最小编辑距离，所以0不能作为判断条件
+    // if (i === 0) return j 
+    // if (j === 0) return i 
     if (memo[i][j]) {
       // console.log(`1memo[${i}][${j}] is`, memo[i][j])
       return memo[i][j]
@@ -31,7 +38,18 @@ function minDistance(word1: string, word2: string): number {
   return result
 };
 
-// dp法  dp(i, j)  # 返回 world1[0..i] 和 world2[0..j] 的最小编辑距离
+/**
+ * dp法  dp(i, j)  # 返回 world1[0..i] 和 world2[0..j] 的最小编辑距离，不关注具体为多少，只关注
+ * if(word1[i]== word2[j] ) 两个值相同的时候，i，j同时移动，dp[i][j] = dp[i-1][j-1]
+ * else dp[i][j] = 1+ min(
+ *   dp[i][j-1],    insert情况，Word1中插入了一个新的元素，那word2的j移动一个元素，继续比较
+ *   dp[i-1][j],    delete情况，word1中删除了一个元素，即i丢失了，i-1移动一个元素，继续和word2比较
+ *  dp[i-1][j-1])   replace情况，将word1中i和i-1换位置，即新的i和之前的j匹配了，那么继续从j-1和新的i-1开始比价
+ * 
+ * 此处的base case为当word1为0时候,即i==0的时候，j直接为需要操作的步骤，同理：
+ * 当word2为0的时候j==0，i为word1直接要删除的步骤
+ */
+
 // function minDistance(word1: string, word2: string): number {
 //   let m = word1.length, n = word2.length
 //   // 此处如果不fill，则无法触发map方法，当为empty时候，自动跳过map了
