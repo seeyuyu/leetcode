112068702e614642a3e3e24597f73c2feae2e034
seeyuyu <seeyuyu@163.com>
2021-03-24 11:37:11 +0800

    feat: 516,dp处理

--- 416.分割等和子集.ts
+++ 416.分割等和子集.ts
@@ -11,7 +11,7 @@ function canPartition(nums: number[]): boolean {
     return item + sum
   }, 0)
   if ((sum % 2)) return false
-  nums = nums.sort((a, b) => a - b)
+  // nums = nums.sort((a, b) => a - b)
   let len = nums.length
   let target = sum / 2
   // 生成一个数组长度为行，target+1 为列的dp表，
@@ -19,24 +19,25 @@ function canPartition(nums: number[]): boolean {
     .map(item => new Array(target + 1).fill(false))
   // 设计dp[i][j]为在0和i之间，存在一些正整数，是的这些和为j
   // 处理边界值
-  if (target >= nums[0]) {
-    dp[0][nums[0]] = true
-  }
-  
+  // if (target >= nums[0]) {
+  //   dp[0][nums[0]] = true
+  // }
+
   for (let i = 1; i < len; i++) {
     for (let j = 0; j < target + 1; j++) {
-      // 先从上一行的结果抄下来，
-      dp[i][j] = dp[i - 1][j]
       if (j === nums[i]) {
         dp[i][j] = true
-        continue;
-      } if (nums[i] < j) {
+        // continue;
+      } else if (j > nums[i]) {
         dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]]
+      } else {
+        dp[i][j] = dp[i - 1][j]
       }
     }
   }
   // console.log(dp)
   return dp[len - 1][target]
 };
+//  "[9,1,2,4,10]"
 // @lc code=end
 
---
+++ 516.最长回文子序列.ts
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=516 lang=typescript
+ *
+ * [516] 最长回文子序列
+ */
+
+// @lc code=start
+function longestPalindromeSubseq(s: string): number {
+  let len = s.length
+  let max = 1
+  let dp = new Array(len).fill(null).map(() => new Array(len).fill(0))
+  // 初始化过程，一定要注意，三种情况，都要考虑到
+  for (let i = 0; i <= len - 1; i++) {
+    dp[i][i] = 1
+    if (i + 1 <= len - 1 && s[i] === s[i + 1]) {
+      dp[i][i + 1] = 2
+      if (max === 1) {
+        max = 2
+      }
+    }else{
+      dp[i][i + 1] = 1
+    }
+  }
+  // console.log('before dp', dp)
+
+  // dp[i][j] 表示i到j之间，最长回文子串的长度
+  for (let i = len - 3; i >= 0; i--) {
+    for (let j = i + 2; j <= len - 1; j++) {
+      if (s[i] === s[j]) {
+        dp[i][j] = dp[i + 1][j - 1] + 2
+      } else {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1])
+      }
+      max = Math.max(max, dp[i][j])
+      // console.log(`dp[${i}][${j}] is`, dp[i][j])
+    }
+  }
+  // console.log('end dp', dp)
+  return max
+};
+//  ""babbbbaabb""
+//  ""aa""
+//  ""abcabcabcabc""
+// @lc code=end
+
