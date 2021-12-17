dd43b34aa5548b811540224a89646f1915a54007
lidongyan1 <lidongyan1@360.cn>
2021-12-17 17:46:26 +0800

    feat: 300最长递增子序列

--- 300.最长递增子序列.ts
+++ 300.最长递增子序列.ts
@@ -22,38 +22,60 @@
 //   return max
 // };
 
-function lengthOfLIS(nums: number[]): number {
-  let length = nums.length
-  let table = []
-  table.push(nums[0])
-  for (let i = 1; i < length; i++) {
-    let cur = nums[i]
-    if (cur > table[table.length - 1]) {
-      table.push(cur)
-    } else {
-      const index = binarySearch(table, cur)
-      // console.log('index is', index)
-      table[index] = cur
-    }
-    // console.log('cur & table is', cur, table)
-  }
-  return table.length
+// function lengthOfLIS(nums: number[]): number {
+//   let length = nums.length
+//   let table = []
+//   table.push(nums[0])
+//   for (let i = 1; i < length; i++) {
+//     let cur = nums[i]
+//     if (cur > table[table.length - 1]) {
+//       table.push(cur)
+//     } else {
+//       const index = binarySearch(table, cur)
+//       // console.log('index is', index)
+//       table[index] = cur
+//     }
+//     // console.log('cur & table is', cur, table)
+//   }
+//   return table.length
+// }
 
+// const binarySearch = (table, num) => {
+//   let min = 0, max = table.length - 1
+//   while (min < max) {
+//     const mid = min + Math.floor((max - min) / 2)
+//     // console.log('mid is', mid)
+//     if (num > table[mid]) {
+//       min = mid + 1
+//     } else {
+//       max = mid
+//     }
+//   }
+//   return min
+// }
+
+function lengthOfLIS(nums: number[]): number {  
+  let dp = new Array(nums.length)
+  // dp【n】定义为以nums[n] 结尾元素递增子序列的结果。
+  // dp[n] 依赖于 之前所有元素的最大值，如果 nums【n】 大于 
+  dp[0] = 1
+  let max = 1
+  for(let i = 1; i <= nums.length; i++){
+    let res = 1
+    for(let j = 0; j<i; j++){
+      // 当前项大于 比较项，是个递增序列，dp[n] 
+      if(nums[i] > nums[j] ){
+        res = Math.max(res, dp[j] +1)
+      }
+    }
+
+    dp[i] = res
+    max = Math.max(dp[i], max)
+  }
+  // console.log(dp)
+  return max
 }
 
-const binarySearch = (table, num) => {
-  let min = 0, max = table.length - 1
-  while (min < max) {
-    const mid = min + Math.floor((max - min) / 2)
-    // console.log('mid is', mid)
-    if (num > table[mid]) {
-      min = mid + 1
-    } else {
-      max = mid
-    }
-  }
-  return min
-}
 
 //    "[3,5,6,2,5,4,19,5,6,7,12]"
 // @lc code=end
