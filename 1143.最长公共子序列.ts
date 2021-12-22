096ead4e5da79ca6cfae6b8cd3dcd101afb073ee
lidongyan1 <lidongyan1@360.cn>
2021-12-22 16:42:35 +0800

    516 dp 最长回文子序列

--- 1143.最长公共子序列.ts
+++ 1143.最长公共子序列.ts
@@ -5,30 +5,68 @@
  */
 
 // @lc code=start
+
+// memo 数组暂存
+// Your runtime beats 5.16 % of typescript submissions
+// Your memory usage beats 20 % of typescript submissions (51.7 MB)
+// function longestCommonSubsequence(text1: string, text2: string): number {
+//   let memo = new Array(text1.length + 1).fill(0)
+//     .map(() => new Array(text2.length + 1).fill(undefined))
+
+//   for (let i = 0; i <= text1.length; i++) {
+//     for (let j = 0; j <= text2.length; j++) {
+//       if (i === 0 || j === 0) {
+//         memo[i][j] = 0
+//       }
+//     }
+//   }
+
+//   // console.log(memo)
+
+//   function dp(i, j) {
+//     if (i === -1 || j === -1) return 0
+//     if(memo[i][j]) return memo[i][j]
+
+//     if (text1[i] === text2[j]) {
+//       memo[i][j] = dp(i - 1, j - 1) + 1
+//       return memo[i][j]
+
+//     } else {
+//       memo[i][j] = Math.max(dp(i - 1, j), dp(i, j - 1))
+//       return memo[i][j]
+//     }
+//   }
+
+//   return dp(text1.length - 1, text2.length - 1)
+// };
+
+
+// Your runtime beats 21.29 % of typescript submissions
+// Your memory usage beats 20.64 % of typescript submissions (51.5 MB)
 function longestCommonSubsequence(text1: string, text2: string): number {
-  let memo = new Array(text1.length + 1).fill(0)
-    .map(() => new Array(text2.length + 1).fill(undefined))
-  // let arr = new Array(3).fill(0).map(_ => new Array(3).fill(0))
+  let dp = new Array(text1.length+1).fill(0).map(()=> new Array(text2.length+1).fill(undefined))
+  // console.log(dp)
+
   for (let i = 0; i <= text1.length; i++) {
     for (let j = 0; j <= text2.length; j++) {
       if (i === 0 || j === 0) {
-        memo[i][j] = 0
+        dp[i][j] = 0
+      }
+    }
+  }
+  for(let i = 1;i<=text1.length; i++){
+    for(let j = 1;j<=text2.length; j++){
+      if(text1[i-1] === text2[j-1]){
+        dp[i][j] = dp[i-1][j-1] +1
+      }else{
+        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
       }
     }
   }
 
-  console.log(memo)
-
-  function dp(i, j) {
-    if (i === -1 || j === -1) return 0
-    if (text1[i] === text2[j]) {
-      // if(memo[i-1, j-1 ], )
-      return dp(i - 1, j - 1) + 1
-    } else {
-      return Math.max(dp(i - 1, j), dp(i, j - 1))
-    }
-  }
-  return dp(text1.length - 1, text2.length - 1)
-};
+ console.log(dp)
+  return dp[text1.length][text2.length]
+}
+// ""abc" \n "def""
 // @lc code=end
 
--- 516.最长回文子序列.ts
+++ 516.最长回文子序列.ts
@@ -21,10 +21,10 @@
 //       dp[i][i + 1] = 1
 //     }
 //   }
-  // console.log('before dp', dp)
+// console.log('before dp', dp)
 
-  
-  // dp[i][j] 表示i到j之间，最长回文子串的长度
+
+// dp[i][j] 表示i到j之间，最长回文子串的长度
 //   for (let i = len - 3; i >= 0; i--) {
 //     for (let j = i + 2; j <= len - 1; j++) {
 //       if (s[i] === s[j]) {
@@ -43,7 +43,35 @@
 //  ""aa""
 //  ""abcabcabcabc""
 
+// 定义 dp[i][j] 为 s[i][j] 之间，最常回文子序列的值
+// baseCase 当 i 和 j 相同的时候，必然是回文子串，i<=j; 所以 dp[i][j] = 1
+// i 和 j 相等的情况，[i+1][j-1] 加2，
+// 不相等的话，从[i+1][j] 或者[i][j-1] 最大值取
+
+// Your runtime beats 42.35 % of typescript submissions
+// Your memory usage beats 28.23 % of typescript submissions (85.8 MB)
 function longestPalindromeSubseq(s: string): number {
+  let len = s.length
+  let dp = new Array(len ).fill(0).map(() => new Array(len ).fill(0))
+
+  // console.log(dp)
+  for (let i = 0; i < len; i++) {
+    dp[i][i] = 1
+  }
+  for (let i = len - 1; i >= 0; i--) {
+    for (let j = i+1; j < len; j++ ) {
+      if (s[i] === s[j]) {
+        dp[i][j] = dp[i + 1][j - 1] + 2
+        // console.log('相等，dp[i][j] is ',i+1,j+1, '-----', dp[i][j])
+      } else {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1])
+        // console.log('不相等，dp[i][j] is ',i+1,j+1, '-----', dp[i][j])
+      }
+    }
+  }
+  // dp[i][j] = max()
+  // console.log(dp)
+  return dp[0][len-1]
 }
 // @lc code=end
 
