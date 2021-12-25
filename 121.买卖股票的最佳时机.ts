fb7b4cc5cf1090c31e5ea0950dc9cd210323b9ee
lidongyan1 <lidongyan1@360.cn>
2021-12-25 22:24:43 +0800

    feat: 121

---
+++ 121.买卖股票的最佳时机.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=typescript
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+// Your runtime beats 25.6 % of typescript submissions
+// Your memory usage beats 48.8 % of typescript submissions (48.2 MB)
+
+function maxProfit(prices: number[]): number {
+  let len = prices.length, max = 0, min = prices[0]
+  // let dp = new Array(len).fill(0).map(( ) => new Array(len))
+  for (let i = 0; i < len; i++) {
+    min = Math.min(prices[i], min)
+    max = Math.max(prices[i] - min, max)
+    // for(let j =i; j<len; j++){
+    //   max = Math.max(prices[j] - prices[i], max)
+    //   // dp[i][j] = Math.max(dp[i][j])
+    // }
+  }
+  return max
+};
+// @lc code=end
+
